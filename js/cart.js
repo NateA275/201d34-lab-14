36dfb11ee7e5378676f580032c625af90b4a2ca6
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-19 14:56:50 -0700

    fixed table clear function

--- js/cart.js
+++ js/cart.js
@@ -17,22 +17,39 @@ function renderCart() {
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {
-  var cartTable = document.getElementById('cart');
-  cartTable.innerHTML = '';
+// Remove all of the rows (tr) in the cart table (tbody)
+function clearCart() { // fix this to only delete things from tbody
+  var cartTableBody = document.getElementsByTagName('tbody')[0];
+  cartTableBody.innerHTML = '';
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-
   // TODO: Find the table body
-
+  var cartTable = document.getElementById('cart');
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  for (var product of Cart) {
+    // TODO: Create a TR
+    var trEL = document.createElement('tr');
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    
 
+    var delTD = document.createElement('td');
+    var delButton = document.createElement('button');
+    delButton.textContent = 'X';
+    delTD.appendChild(delButton);
+
+    
+
+    
+
+
+    var quantity = document.createElement('td');
+    var itemName = document.createElement('td');
+    
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  }
+    
 }
 
 function removeItemFromCart(event) {
